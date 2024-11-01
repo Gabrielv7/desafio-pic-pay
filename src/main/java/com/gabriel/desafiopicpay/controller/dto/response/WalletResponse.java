b0007ba69711484c82c89e3f939c26c52b1333b9
Gabriel Costa <gabrielcosta.v7@gmail.com>
2024-11-01 15:31:33 -0300

    fix: Trocando o atributo balance para BigDecimal

--- src/main/java/com/gabriel/desafiopicpay/controller/dto/response/WalletResponse.java
+++ src/main/java/com/gabriel/desafiopicpay/controller/dto/response/WalletResponse.java
@@ -1,7 +1,8 @@
 package com.gabriel.desafiopicpay.controller.dto.response;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public record WalletResponse(UUID id,
-                             Integer balance) {
+                             BigDecimal balance) {
 }
