5a4c5415a18f6cbe17ad55e6cd5db2ecc94f3a29
Gabriel Costa <gabrielcosta.v7@gmail.com>
2024-11-23 09:02:35 -0300

    cleanup: removendo método da interface nnão usado

--- src/main/java/com/gabriel/desafiopicpay/repository/TransactionRepository.java
+++ src/main/java/com/gabriel/desafiopicpay/repository/TransactionRepository.java
@@ -2,15 +2,11 @@ package com.gabriel.desafiopicpay.repository;
 
 import com.gabriel.desafiopicpay.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
-
-    @Query(value = "select * from tb_transaction where payer = :userId and creation_date = :creationDate", nativeQuery = true)
-    Transaction findTransactionByUserId(UUID userId, LocalDateTime creationDate);
 
 }
